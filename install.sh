c54975f13db7436fb0298fb8bf7aa4344317f2b1
ZhuzhuNo3 <907130054@qq.com>
2020-11-11 17:26:07 +0800

    [bugfix]在tmux中粘贴文本至vim中会导致缩进错误

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ ! -f tmux.conf ]; then
+CONF_PATH=`dirname $0`
+if [ ! -f "${CONF_PATH}/tmux.conf" ]; then
     exit 1
 fi
-if [ -f ~/.tmux.conf ]; then
+if [[ -f ~/.tmux.conf || -L ~/.tmux.conf ]]; then
     echo "found ~/.tmux.conf, need to backup?(y/n)"
     read _tag 
     if [[ x$_tag == xy || x$_tag == x ]]; then
@@ -11,7 +12,7 @@ if [ -f ~/.tmux.conf ]; then
         rm ~/.tmux.conf
     fi
 fi
-ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
-if [ -d ~/.tmux/plugins ]; then
+ln -s "${CONF_PATH}/tmux.conf" ~/.tmux.conf
+if [[ ! -d ~/.tmux/plugins || ! -d ~/.tmux/plugins/tmux-pane-ssh-title ]]; then
     mkdir -p ~/.tmux/plugins && git clone git@github.com:ZhuzhuNo3/tmux-pane-ssh-title.git ~/.tmux/plugins/tmux-pane-ssh-title
 fi
